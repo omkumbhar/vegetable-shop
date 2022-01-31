d6816b73041222b7b35d5020d20d8a7ff56e041c
Kumbhar <E2011817@nventco.com>
2022-01-31 16:50:36 +0530

    Developed simple login Form

--- client/src/app/Componets/login-form/login-form.component.ts
+++ client/src/app/Componets/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,14 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  user = {username :"user@gmail.com" , password : "123456"  };
 
-  constructor() { }
+  admin = {username :"admin@gmail.com" , password : "123456"  };
+  
+  selectedRoleIndex : number = 0; 
 
-  ngOnInit(): void {
+  dropDownOptions :  any[ ] ;
+
+  myForm : FormGroup;
+  usernameController : FormControl;
+  password : FormControl;
+
+  constructor() { 
+    this.dropDownOptions = [{id : 0 , role : "Admin" }, {id : 1,role : "User"}] ;
+    this.usernameController = new FormControl('',[Validators.required,Validators.email]);
+    this.password = new FormControl('',Validators.required);
+    
+    this.myForm = new FormGroup(
+      {
+        username : this.usernameController,
+        password : this.password
+      }
+    );
+    
   }
 
+  ngOnInit(): void {
+   
+    
+  }
+
+  
+  
+
+
   login(username : string, password :string){
-    console.log(`${username} ${password}`);
+    if ( this.selectedRoleIndex && this.user.username === username 
+            && this.user.password === password){
+              
+      console.log(`${username} ${password} User login succesful`);    
+    }
+    else if(this.selectedRoleIndex === 0 && this.admin.username === username 
+      && this.admin.password === password  )
+      
+      console.log(`${username} ${password} Admin login succesful`);    
+
   }
   getColor(){
     return "red";
