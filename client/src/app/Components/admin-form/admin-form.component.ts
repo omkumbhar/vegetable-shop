8354cd43c276a3d97bad7e605a5815ffbd1415ca
omkumbhar <om.kumbhar1998@gmail.com>
2022-05-08 09:05:16 +0000

    Added validations for admin form

--- client/src/app/Components/admin-form/admin-form.component.ts
+++ client/src/app/Components/admin-form/admin-form.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ItemModel } from 'src/app/DataModels/ItemModel';
 
 @Component({
   selector: 'app-admin-form',
   templateUrl: './admin-form.component.html',
-  styleUrls: ['./admin-form.component.css']
+  styleUrls: ['./admin-form.component.css'],
 })
-export class AdminFormComponent implements OnInit {
-  formTitle = "ADD ITEM";
-  itemType = [ {id : 0, type :"Vegitable"},{id : 1, type :"Fruit"}];
-  constructor() { }
-
-  ngOnInit(): void {
+export class AdminFormComponent {
+  formTitle = 'ADD ITEM';
+  itemType = [
+    { id: 0, type: 'Vegitable' },
+    { id: 1, type: 'Fruit' },
+  ];
+  items: ItemModel[] = [];
+  constructor() {}
+  onSubmit(value: ItemModel) {
+    console.log('value = ' + value);
+    console.log('itemName = ' + value.itemName);
+    console.log('itemGroup = ' + value.itemGroup);
+    console.log('itemQuantity = ' + value.itemQuantity);
+    console.log('itemType = ' + value.itemType);
   }
-
 }
