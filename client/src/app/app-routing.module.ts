6f3833e02032d169ad75f3b18d27e08c91d387ee
Omkar, Kumbhar <Omkar.Kumbhar@nvent.com>
2022-06-14 09:40:24 +0000

    Added seprate directory for admin components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminFormComponent } from 'src/app/Components/admin-form/admin-form.component';
+import { AdminFormComponent } from 'src/app/Components/admin-components/components/admin-form/admin-form.component';
 import { LoginFormComponent } from 'src/app/Components/login-form/login-form.component';
-import { AdminHomeComponent } from 'src/app/Components/admin-home/admin-home.component';
+import { AdminHomeComponent } from 'src/app/Components/admin-components/components/admin-home/admin-home.component';
 
 const routes: Routes = [
   { path: '', component: LoginFormComponent },
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -51,7 +51,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { AdminNavComponent } from './Components/admin-nav/admin-nav.component';
+import { AdminNavComponent } from './Components/admin-components/components/admin-nav/admin-nav.component';
 
 @NgModule({
   declarations: [AppComponent, routingComponents, AdminNavComponent],
