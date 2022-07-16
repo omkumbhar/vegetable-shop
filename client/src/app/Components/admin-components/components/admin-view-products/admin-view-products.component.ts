6fc235324f284b9de4bb278c6911826239778e12
omkumbhar <om.kumbhar1998@gmail.com>
2022-07-16 20:02:36 +0530

    Added table for item view for admin

--- client/src/app/Components/admin-components/components/admin-view-products/admin-view-products.component.ts
+++ client/src/app/Components/admin-components/components/admin-view-products/admin-view-products.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ItemModel } from 'src/app/DataModels/ItemModel';
+import { ItemService } from 'src/app/Services/item.service';
 
 @Component({
   selector: 'app-admin-view-products',
   templateUrl: './admin-view-products.component.html',
   styleUrls: ['./admin-view-products.component.css']
 })
-export class AdminViewProductsComponent implements OnInit {
+export class AdminViewProductsComponent {
+  displayedColumns: string[] = ['name', 'type', 'quantity', 'price', 'action'];
+  dataSource: ItemModel[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private itemService: ItemService) {
+    this.dataSource = itemService.getItemList();
   }
 
 }
